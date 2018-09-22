3350f9c43027513f6a172fe96aca631bcb38446a
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-09-22 14:45:04 -0500

    #21 Location constructor in Models

---
+++ Models/location.js
@@ -0,0 +1,28 @@
+const db = require('../DB');
+
+class Location {
+  constructor(id, ownerUserId, active, lattitude, longitude,
+    street, colony, postalCode, streetAcross1, streetAcross2,
+    extNum, intNum, numRooms, availableRooms) {
+    this.id = id;
+    this.ownerUserId = ownerUserId;
+    this.active = active;
+    this.lattitude = lattitude;
+    this.longitude = longitude;
+    this.street = street;
+    this.colony = colony;
+    this.postalCode = postalCode;
+    this.streetAcross1 = streetAcross1;
+    this.streetAcross2 = streetAcross2;
+    this.extNum = extNum;
+    this.intNum = intNum;
+    this.numRooms = numRooms;
+    this.availableRooms = availableRooms;
+  }
+
+  save() {
+    db.insert(this);
+  }
+}
+
+module.exports = Location;
