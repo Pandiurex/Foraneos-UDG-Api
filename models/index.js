80e0b848f5e132e31d2dc882a5540ad6fc802c74
Jose Aguayo <jmiguel.aguayo@alumnos.udg.mx>
2018-09-30 10:51:37 -0500

    routes were added comlaint, livesIn, locationImage, email

--- models/index.js
+++ models/index.js
@@ -2,3 +2,7 @@ exports.users = require('./usersMdl');
 exports.locations = require('./locationsMdl');
 exports.rates = require('./ratesMdl');
 exports.messages = require('./messagesMdl');
+exports.complaint = require('./complaintMdl');
+exports.livesIn = require('./livesInMdl');
+exports.locationImage = require('./locationImageMdl');
+exports.email = require('./emailMdl');
