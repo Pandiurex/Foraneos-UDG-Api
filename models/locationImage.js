07b46d0602c1a1d3393f65aec2d51b42662b2bc0
Jose Aguayo <jmiguel.aguayo@alumnos.udg.mx>
2018-09-30 10:54:11 -0500

    locationImage class id modified

---
+++ models/locationImage.js
@@ -0,0 +1,10 @@
+class LocationImage {
+  constructor(data) {
+    this.id = data.id;
+    this.locationId = data.locationId;
+    this.locationImage = data.locationImage;
+    this.description = data.description;
+  }
+}
+
+module.exports = LocationImage;
