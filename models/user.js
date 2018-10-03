48732696ce93b3f22c18e953c51bf28efc0bbab7
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-10-03 14:55:14 -0500

    way to check undefined changed

--- models/user.js
+++ models/user.js
@@ -1,29 +1,21 @@
 class User {
   constructor(data) {
-    if (data.id !== undefined) { this.id = data.id; }
-    if (data.mainEmailId !== undefined) { this.mainEmailId = data.mainEmailId; }
-    if (data.userType !== undefined) { this.userType = data.userType; }
-    if (data.username !== undefined) { this.username = data.username; }
-    if (data.password !== undefined) { this.password = data.password; }
-    if (data.name !== undefined) { this.name = data.name; }
-    if (data.firstSurname !== undefined) { this.firstSurname = data.firstSurname; }
-    if (data.secondSurname !== undefined) { this.secondSurname = data.secondSurname; }
-    if (data.profileImage !== undefined) {
-      if (typeof data.profileImage === 'string') {
-        this.profileImage = Buffer.from(data.profileImage);
-      } else {
-        this.profileImage = JSON.stringify(data.profileImage);
-      }
-    }
-    if (data.birthdate !== undefined) {
-      this.birthYear = data.birthdate.getFullYear();
-      this.birthMonth = data.birthdate.getMonth();
-      this.birthDay = data.birthdate.getDay();
-    }
-    if (data.birthYear !== undefined) { this.birthYear = data.birthYear; }
-    if (data.birthMonth !== undefined) { this.birthMonth = data.birthMonth; }
-    if (data.birthDay !== undefined) { this.birthDay = data.birthDay; }
-    if (data.gender !== undefined) { this.gender = data.gender; }
+    this.id = data.id;
+    this.mainEmailId = data.mainEmailId;
+    this.active = data.active;
+    this.userType = data.userType;
+    this.username = data.username;
+    this.password = data.password;
+    this.name = data.name;
+    this.firstSurname = data.firstSurname;
+    this.secondSurname = data.secondSurname;
+    this.profileImage = data.secondSurname;
+    this.birthdate = data.birthDay;
+    this.gender = data.gender;
+
+    Object.keys(this).forEach((key) => {
+      if (this[key] === undefined) { delete this[key]; }
+    });
   }
 
   setMainEmail(email) {
