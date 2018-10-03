120136355feae806d6f22cacabcd9f97455e3c45
AlfredoCU <alfredo_11_cu@hotmail.com>
2018-10-02 19:54:50 -0500

    Se modifico las rutas y se quitaron rutas no necesarias

--- routes/livesInRoute.js
+++ routes/livesInRoute.js
@@ -8,10 +8,8 @@ const route = express.Router();
 
 route
   .get('/', livesInController.showAll)
-  .get('/:id', livesInController.showOne)
   .post('/', livesInController.create)
-  .put('/:id', livesInController.update)
-  .delete('/:id', livesInController.remove)
-  .patch('/:id', livesInController.patch);
+  .put('/:livesInId', livesInController.update)
+  .patch('/:livesInId', livesInController.patch);
 
 module.exports = route;
