7ab275a52e8ce5cad2b671817bd2e4158f7e9688
AlfredoCU <alfredo_11_cu@hotmail.com>
2018-10-02 20:00:44 -0500

    Se agrego un nuevo controlador

---
+++ controllers/livesController.js
@@ -0,0 +1,16 @@
+// Controllers of Lives.
+exports.showAll = (req, res) => {
+  res.send('Show all Lives');
+};
+
+exports.create = (req, res) => {
+  res.send(req.body);
+};
+
+exports.update = (req, res) => {
+  res.send(req.body);
+};
+
+exports.patch = (req, res) => {
+  res.send(req.body);
+};
