70330570d2877d86b15945e0d64488291d3509d0
AlfredoCU <alfredo_11_cu@hotmail.com>
2018-10-02 20:57:57 -0500

    Se agrago una expresion regular para validar los las id en las rutas antes de llegar a los middlewares y controladores

--- routes/locationsRoute.js
+++ routes/locationsRoute.js
@@ -6,10 +6,10 @@ const route = express.Router();
 
 route
   .get('/', locationsController.showAll)
-  .get('/:locationId', locationsController.showOne)
+  .get('/:locationId([0-9]+)', locationsController.showOne)
   .post('/', locationsController.create)
-  .put('/:locationId', locationsController.update)
-  .patch('/:locationId', locationsController.patch)
-  .delete('/:locationId', locationsController.remove);
+  .put('/:locationId([0-9]+)', locationsController.update)
+  .patch('/:locationId([0-9]+)', locationsController.patch)
+  .delete('/:locationId([0-9]+)', locationsController.remove);
 
 module.exports = route;
