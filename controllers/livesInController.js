d4f797de3b37c76c186c8a95261cf645fb2e3d94
AlfredoCU <alfredo_11_cu@hotmail.com>
2018-10-02 19:43:31 -0500

    Se quitaron métodos que no son necesarios

--- controllers/livesInController.js
+++ controllers/livesInController.js
@@ -3,19 +3,11 @@ exports.showAll = (req, res) => {
   res.send('Show all Lives In');
 };
 
-exports.showOne = (req, res) => {
-  res.send(req.params.id);
-};
-
 exports.create = (req, res) => {
   res.send(req.body);
 };
 
 exports.update = (req, res) => {
-  res.send(req.body);
-};
-
-exports.remove = (req, res) => {
   res.send(req.body);
 };
 
