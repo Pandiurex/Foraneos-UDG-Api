ba2d18e4227408282edc8a1e393f984bfc119fd1
AlfredoCU <alfredo_11_cu@hotmail.com>
2018-10-02 19:32:12 -0500

    Se modifico el controllador de complaints quitando métodos que no son necesarios

--- controllers/complaintsController.js
+++ controllers/complaintsController.js
@@ -3,22 +3,10 @@ exports.showAll = (req, res) => {
   res.send('Show all Complaints');
 };
 
-exports.showOne = (req, res) => {
-  res.send(req.params.id);
-};
-
 exports.create = (req, res) => {
   res.send(req.body);
 };
 
-exports.update = (req, res) => {
-  res.send(req.body);
-};
-
 exports.remove = (req, res) => {
-  res.send(req.body);
-};
-
-exports.patch = (req, res) => {
   res.send(req.body);
 };
