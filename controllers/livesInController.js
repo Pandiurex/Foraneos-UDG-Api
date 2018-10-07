f802629e73f6f126a03071591c4ac384802a4db7
Cristopher Estrada <crissin21_01@hotmail.com>
2018-10-07 18:03:29 -0500

    Lives in controller was created

--- controllers/livesInController.js
+++ controllers/livesInController.js
@@ -1,16 +1,95 @@
 // Controllers of Lives-In.
-exports.showAll = (req, res) => {
-  res.send('Show all Lives In');
+
+const {
+  livesIn,
+} = require('../models');
+
+exports.showAll = async (req, res) => {
+  let result = await livesIn.getAll();
+
+  if (result === 0) {
+    result = {
+      error: {
+        status: 404,
+        message: 'Resource not found',
+      },
+    };
+    res.status(404);
+  }
+
+  res.send(result);
 };
 
-exports.create = (req, res) => {
-  res.send(req.body);
+exports.create = async (req, res) => {
+  let result = await livesIn.create(req.body);
+
+  if (result === 0) {
+    result = {
+      error: {
+        status: 409,
+        message: 'Conflict creating resource',
+      },
+    };
+    res.status(409);
+  } else if (result === 1) {
+    result = {
+      error: {
+        status: 409,
+        message: 'Lives In already exists',
+      },
+    };
+    res.status(409);
+  } else {
+    res.status(201);
+  }
+
+  res.send(result);
 };
 
-exports.update = (req, res) => {
-  res.send(req.body);
+exports.update = async (req, res) => {
+  let result = await livesIn.update(req.params.id, req.body);
+
+  if (result === 0) {
+    result = {
+      error: {
+        status: 409,
+        message: 'Error updating resource',
+      },
+    };
+    res.status(409);
+  } else if (result === 1) {
+    result = {
+      error: {
+        status: 409,
+        message: 'New email doesn\'t belong to the user',
+      },
+    };
+    res.status(409);
+  }
+
+  res.send(result);
 };
 
-exports.patch = (req, res) => {
-  res.send(req.body);
+exports.patch = async (req, res) => {
+  let result = await livesIn.patch(req.params.id, req.body);
+
+  if (result === 0) {
+    result = {
+      error: {
+        status: 409,
+        message: 'Error updating resource',
+      },
+    };
+    res.status(409);
+  } else if (result === 1) {
+    result = {
+      error: {
+        status: 409,
+        message: 'Doesn\'t can reduce more the num of rooms',
+      },
+    };
+    res.status(409);
+  }
+
+  res.send(result);
 };
