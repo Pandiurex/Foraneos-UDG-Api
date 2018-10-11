d065625ab19ef6d26cf6e07f30e2c051fe575930
Jose Aguayo <jmiguel.aguayo@alumnos.udg.mx>
2018-10-11 10:27:37 -0500

    create model with create and get

---
+++ models/token.js
@@ -0,0 +1,52 @@
+const db = require('../db');
+
+class Token {
+  constructor(data) {
+    this.id = data.id;
+    this.nToken = data.nToken;
+    this.createAt = data.ceateAt;
+    this.expire = data.expires;
+    this.status = data.status;
+    this.userId = data.userId;
+  }
+
+  static async get(tokenId) {
+    let tokenTbl = '';
+    try {
+      tokenTbl = await db.select('token', '',
+        [{ col: 'id', oper: '=', val: tokenId }]);
+    } catch (e) {
+      return '';
+    }
+
+    if (tokenTbl.length === 0) { return 0; }
+
+    const token = this.processResult(tokenTbl)[0];
+
+    return JSON.stringify(token);
+  }
+
+  static async create({id, nToken, creatAt, expires, status, userId }) {
+    let tokenId = '';
+    try {
+      tokenId = await db.insert('token',
+        ['nToken', 'creatAt', 'expires', 'status', 'userId'],
+        [nToken, creatAt, expires, status, userId]);
+    } catch (e) {
+      return '';
+    }
+
+    return this.get(tokenId);
+  }
+
+  static processResult(data) {
+    this.result = [];
+    data.forEach((obj) => {
+      this.result.push(new Token(obj));
+    });
+    return this.result;
+  }
+
+}
+
+module.exports = Token;
