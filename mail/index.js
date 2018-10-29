579f4d1e40308fa3c78b3ea01f5fce5e669820a1
Cristopher Estrada <crissin21_01@hotmail.com>
2018-10-28 20:22:23 -0600

    Se agrego la verificacion de coneccion y el envio de mensaje

--- mail/index.js
+++ mail/index.js
@@ -10,11 +10,23 @@ class Mailer {
         user: process.env.MAIL_USER, // generated ethereal user
         pass: process.env.MAIL_PASS, // generated ethereal password
       },
+      tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false,
+      },
     });
     this.mailOptions = {
       from: '"Testing Mail" <foraneos@udg.com',
     };
+    this.transporter.verify((error, success) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Server is ready to take our messages');
+      }
+    });
   }
+
 
   // Validar que el transporter si tiene una conexion
   // this.transporter.verify()
