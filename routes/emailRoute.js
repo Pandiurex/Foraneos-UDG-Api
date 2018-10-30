569667a02d5d03e821b41c1a3dc2727ba09a33b3
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-10-29 23:04:56 -0600

    Middlewares modificados para envio de correo en emailRoute

--- routes/emailRoute.js
+++ routes/emailRoute.js
@@ -3,11 +3,12 @@ const express = require('express');
 const {
   emailController,
 } = require('../controllers');
+const { authMid } = require('../middlewares');
 
 const route = express.Router();
 
 route
-  .post('/', emailController.create)
+  .post('/', [authMid.sessionChecker, emailController.create], authMid.reqConfirmEmail)
   .delete('/:id', emailController.remove);
 
 module.exports = route;
