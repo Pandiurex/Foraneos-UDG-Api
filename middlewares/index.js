1e21131f0c91174e2ebbe00f12894938c4551cb4
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-12-02 13:44:06 -0600

    Nuevo index de middlewares y eliminascion de errMid

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,8 @@
-exports.errMid = require('./errMid');
+// exports.errMid = require('./errMid');
 exports.authMid = require('./auth');
 exports.complaintMid = require('./complaint');
 exports.emailMid = require('./email');
+exports.factoryMid = require('./factory');
 exports.livesMid = require('./lives');
 exports.locationMid = require('./location');
 exports.locationImageMid = require('./locationImage');
@@ -9,12 +10,13 @@ exports.locationServiceMid = require('./locationService');
 exports.messageMid = require('./message');
 exports.rateMid = require('./rate');
 exports.userMid = require('./user');
+exports.fileMid = require('./file');
 exports.generalMid = require('./general');
 
 
 exports.errorHandler = (err, req, res, next) => {
-  console.log('Error handler');
   if (err) {
-    res.status(err.status || 500).send(err);
+    res.status(err.status || 500).send({ error: err });
   }
+  next();
 };
