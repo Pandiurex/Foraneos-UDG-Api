9fa9e50b8ce5f53a83516f762d95f338a17827b0
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-12-02 13:32:04 -0600

    Middlewares de uso general

--- middlewares/general.js
+++ middlewares/general.js
@@ -1,22 +1,27 @@
+const bcrypt = require('bcrypt');
+
 const getCompare = {
-  word: /^[a-zA-Z_áéíóúñ\s]*$/,
-  email: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/,
+  username: /^[a-zA-Z_0-9]*$/,
+  word: /^[a-zA-Z_áéíóúñÁÉÍÓÚÑ\s]*$/,
+  paragraph: /^[a-zA-Z_áéíóúñÁÉÍÓÚÑ0-9\s\\.,;/\-:'"()!¡?¿*]*$/,
+  email: /[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/,
   password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,20}$/,
+  binary: /^[01]$/,
   number: /^([0-9])+$/,
   decimal: /^\d+\.\d{0,2}$/,
   latLon: /^(\d*\.)?\d+$/,
   postal: /^([0-9]){1,5}$/,
+  locationImage: /^locationImages\/file-[a-z0-9]+\.(jpg|jpeg|png)$/,
+  serviceImage: /^serviceImages\/file-[a-z0-9]+\.(jpg|jpeg|png)$/,
+  profileImage: /^profileImages\/file-[a-z0-9]+\.(jpg|jpeg|png)$/,
 };
 
 const checkParamId = (req, res, next) => {
   if (getCompare.number.test(req.params.id) === false) {
-    const result = {
-      error: {
-        status: 406,
-        message: 'Invalid format in param id',
-      },
-    };
-    res.status(406).send(result);
+    next({
+      status: 406,
+      message: 'Invalid format in param id',
+    });
   } else {
     next();
   }
@@ -24,13 +29,10 @@ const checkParamId = (req, res, next) => {
 
 const checkParamUserId = (req, res, next) => {
   if (getCompare.number.test(req.params.userId) === false) {
-    const result = {
-      error: {
-        status: 406,
-        message: 'Invalid format in param userId',
-      },
-    };
-    res.status(406).send(result);
+    next({
+      status: 406,
+      message: 'Invalid format in param userId',
+    });
   } else {
     next();
   }
@@ -38,15 +40,39 @@ const checkParamUserId = (req, res, next) => {
 
 const checkParamLocationId = (req, res, next) => {
   if (getCompare.number.test(req.params.locationId) === false) {
-    const result = {
-      error: {
-        status: 406,
-        message: 'Invalid format in param locationId',
-      },
-    };
-    res.status(406).send(result);
+    next({
+      status: 406,
+      message: 'Invalid format in param locationId',
+    });
   } else {
     next();
+  }
+};
+
+const checkDate = (date) => {
+  if (date === undefined) {
+    return false;
+  }
+
+  const arr = date.split('-');
+  const auxDate = new Date(arr[0], arr[1] - 1, arr[2]);
+
+  if (arr.length === 3 && auxDate
+   && Number(arr[0]) === auxDate.getFullYear()
+   && Number(arr[1] - 1) === auxDate.getMonth()
+   && Number(arr[2]) === auxDate.getDate()) {
+    return true;
+  }
+  return false;
+};
+
+const hashPassword = async (req) => {
+  if (req.query.password) {
+    req.query.password = await bcrypt.hash(`${req.query.password}`,
+      Number(process.env.SECRET));
+  } else if (req.body.password) {
+    req.body.password = await bcrypt.hash(`${req.body.password}`,
+      Number(process.env.SECRET));
   }
 };
 
@@ -55,4 +81,6 @@ module.exports = {
   checkParamId,
   checkParamUserId,
   checkParamLocationId,
+  checkDate,
+  hashPassword,
 };
