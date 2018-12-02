be408670b6d212103b889c879fc77d4ca57e0e64
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-12-02 13:31:37 -0600

    Nuevos middlewares en ruta email

--- routes/emailRoute.js
+++ routes/emailRoute.js
@@ -1,15 +1,17 @@
 // Routes of email.
 const express = require('express');
-const {
-  emailController,
-} = require('../controllers');
-const { authMid } = require('../middlewares');
+const { emailController } = require('../controllers');
+const { authMid, emailMid, generalMid } = require('../middlewares');
 
 const route = express.Router();
 
 route
-  .post('/', [authMid.sessionChecker, authMid.havePermissions, emailController.create],
-    authMid.reqConfirmEmail)
-  .delete('/:id', emailController.remove);
+  .post('/', [authMid.sessionChecker,
+    authMid.havePermissions,
+    emailMid.checkAll,
+    emailController.create],
+  authMid.reqConfirmEmail)
+  .delete('/:id', [generalMid.checkParamId],
+    emailController.remove);
 
 module.exports = route;
