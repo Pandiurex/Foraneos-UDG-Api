8ce160c3e3f877fe898123381cfa5084211412bb
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-12-03 23:03:39 -0600

    Controlador de complaintTypes documentado

--- controllers/complaintTypesController.js
+++ controllers/complaintTypesController.js
@@ -1,5 +1,12 @@
 const { ComplaintType } = require('../models');
 
+/**
+ * Gets all the complaint types
+ * @param  {object}   req   Request form express package
+ * @param  {object}   res   Response from express package
+ * @param  {Function} next  Function that continues the middlewares processing
+ * @return undefined        Sends error if it happens, otherwise sends all the complaint types
+ */
 exports.showAll = async (req, res, next) => {
   const result = await ComplaintType.getAll();
 
