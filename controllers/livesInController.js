c1494226b26ef4ba107d8519d012f167c7ee0ae8
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-12-03 23:17:32 -0600

    Controlador livesIn documentado

--- controllers/livesInController.js
+++ controllers/livesInController.js
@@ -1,5 +1,12 @@
 const { LivesIn } = require('../models');
 
+/**
+ * Gets all the livesIn related to the userId in req.params
+ * @param  {object}   req   Request form express package
+ * @param  {object}   res   Response from express package
+ * @param  {Function} next  Function that continues the middlewares processing
+ * @return undefined        Sends error if it happens, otherwise sends all the relations
+ */
 exports.showAll = async (req, res, next) => {
   const result = await LivesIn.getAll(req.params.userId);
 
