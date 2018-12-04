45503d74f25f3702664f4bbf92ab7a0713dc85e6
JoseEmmanuelGC <emmanuel_12_gc@hotmail.com>
2018-12-03 23:19:19 -0600

    Controlador locationImage documentado

--- controllers/locationImageController.js
+++ controllers/locationImageController.js
@@ -1,5 +1,12 @@
 const { LocationImage } = require('../models');
 
+/**
+ * Creates a locationImage with the req.body
+ * @param  {object}   req   Request form express package
+ * @param  {object}   res   Response from express package
+ * @param  {Function} next  Function that continues the middlewares processing
+ * @return undefined         Sends error if it happens otherwise sends the locationImage created
+ */
 exports.create = async (req, res, next) => {
   const result = await LocationImage.create(req.body);
 
@@ -13,6 +20,13 @@ exports.create = async (req, res, next) => {
   }
 };
 
+/**
+ * Removes a locationImage with the req.params.id
+ * @param  {object}   req   Request form express package
+ * @param  {object}   res   Response from express package
+ * @param  {Function} next  Function that continues the middlewares processing
+ * @return undefined         Sends error if it happens otherwise sends the complaint removed
+ */
 exports.remove = async (req, res, next) => {
   const result = await LocationImage.remove(req.params.id);
 
